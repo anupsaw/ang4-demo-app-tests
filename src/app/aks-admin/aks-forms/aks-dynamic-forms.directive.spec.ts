f6c4b0d2e8c90a28c1a34bba541cd6fbb6a58021
Anup Saw <anupsaw@gmail.com>
2017-12-23 22:34:25 +0530

    deleted few files

--- src/app/aks-admin/aks-forms/aks-dynamic-forms.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AksDynamicFormsDirective } from './aks-dynamic-forms.directive';
-
-describe('AksDynamicFormsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AksDynamicFormsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
